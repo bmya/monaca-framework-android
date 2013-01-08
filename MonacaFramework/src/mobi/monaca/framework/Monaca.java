57e03c2e1530d356dcb445f5d9af2ecbffcb724d
Kruy Vanna <kruyvanna@gmail.com>
2013-01-08 19:15:17 +0900

    remove unused constants

--- MonacaFramework/src/mobi/monaca/framework/Monaca.java
+++ MonacaFramework/src/mobi/monaca/framework/Monaca.java
@@ -4,13 +4,4 @@ public class Monaca {
 
 	public static final String URL_PARAM = "monaca.url";
 
-    public static final String DEBUG_KEY_PARAM = "monaca.debug_key";
-
-    public static final String DAV_DOMAIN = "dav.monaca.mobi";
-    public static final String IDE_DOMAIN = "stg-ide.monaca.mobi";
-
-    public static final String IDE_HOST = "https://" + IDE_DOMAIN;
-    public static final String DAV_HOST = "https://" + DAV_DOMAIN;
-
-    public static final String DAV_AUTH_NAME = "Monaca DAV Authentication";
 }
