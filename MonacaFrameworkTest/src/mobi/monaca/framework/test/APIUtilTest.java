ad5b43118e9eadd64b3562400e095a8912bee697
Hajime Nakajima <nakajima@asial.co.jp>
2013-03-12 18:52:43 +0900

    rename and added APIUtilTest

--- MonacaFrameworkTest/src/mobi/monaca/framework/test/APIUtilTest.java
+++ MonacaFrameworkTest/src/mobi/monaca/framework/test/APIUtilTest.java
@@ -10,7 +10,7 @@ import mobi.monaca.utils.APIUtil;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
-public class APIUtilTestCase extends TestCase {
+public class APIUtilTest extends TestCase {
 
 	public void testGetQuery() {
 		List<NameValuePair> list = new ArrayList<NameValuePair>();
@@ -29,5 +29,15 @@ public class APIUtilTestCase extends TestCase {
 			fail();
 		}
 
+		List<NameValuePair> list2 = new ArrayList<NameValuePair>();
+		list2.add(new BasicNameValuePair("reg&/id", "hoge/foo"));
+
+		try {
+			String query = APIUtil.getQuery(list2);
+			assertEquals("reg%26%2Fid=hoge%2Ffoo", query); // should be encoded and no &mark
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 }
