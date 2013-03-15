d7600544a3307e957b3bf76ce607a953c4aa42f7
Hajime Nakajima <nakajima@asial.co.jp>
2013-03-15 17:45:58 +0900

    remove unused AssetUriUtil method and test

--- MonacaFrameworkTest/src/mobi/monaca/framework/test/AssetUriUtilTest.java
+++ MonacaFrameworkTest/src/mobi/monaca/framework/test/AssetUriUtilTest.java
@@ -4,21 +4,6 @@ import mobi.monaca.framework.util.AssetUriUtil;
 import android.test.AndroidTestCase;
 
 public class AssetUriUtilTest extends AndroidTestCase {
-
-	public void testIsAssetUriMethod() {
-		assertTrue(!AssetUriUtil.isAssetUri("file:///"));
-		assertTrue(!AssetUriUtil.isAssetUri("file:///android_asset"));
-		assertTrue(AssetUriUtil.isAssetUri("file:///android_asset/"));
-		assertTrue(AssetUriUtil.isAssetUri("file:///android_asset/hoge"));
-	}
-
-	public void testResolveMethod() {
-		eq("file:///fuga", AssetUriUtil.resolve("file:///hoge", "./fuga"));
-		eq("file:///hoge/fuga", AssetUriUtil.resolve("file:///hoge/", "./fuga"));
-		eq("file:///fuga", AssetUriUtil.resolve("file:///hoge/", "../fuga"));
-		eq("file:///hoge/fuga/hoge", AssetUriUtil.resolve("file:///hoge/", "fuga/hoge"));
-	}
-
 	public void testExistsAssetMethod() {
 		assertTrue(AssetUriUtil.existsAsset(mContext, "hoge"));
 		assertTrue(AssetUriUtil.existsAsset(mContext, "hoge.html"));
@@ -44,11 +29,6 @@ public class AssetUriUtilTest extends AndroidTestCase {
 		}
 	}
 	**/
-
-	public void testNormalizeMethod() {
-		eq("file:///fuga", AssetUriUtil.normalize("file:///hoge/../fuga"));
-		eq("file:///hoge/fuga", AssetUriUtil.normalize("file:///hoge/./fuga"));
-	}
 
 	protected void eq(String expected, String actual) {
 		LineComparisonFailure.doAssert(expected, actual);
