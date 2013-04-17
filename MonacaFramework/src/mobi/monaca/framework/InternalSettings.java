fad1778e488ec13f845d913a6a00b7927da1907e
Kruy Vanna <kruyvanna@gmail.com>
2013-04-17 18:51:23 +0900

    fix NPE in InternalSettings

--- MonacaFramework/src/mobi/monaca/framework/InternalSettings.java
+++ MonacaFramework/src/mobi/monaca/framework/InternalSettings.java
@@ -1,17 +1,26 @@
 package mobi.monaca.framework;
 
+import mobi.monaca.framework.util.MyLog;
 import android.os.Bundle;
 
 /** Internal settings for monaca framework **/
 public class InternalSettings {
     
-    public final String DISABLE_UI_CONTAINER_BORDER = "monaca.disableUIContainerBorder";
+    private static final String TAG = InternalSettings.class.getSimpleName();
+	public final String DISABLE_UI_CONTAINER_BORDER = "monaca.disableUIContainerBorder";
     public final String FORCE_DISABLE_WEBVIEW_GPU = "monaca.forceDisableWebviewGPU";
     
     public final boolean disableUIContainerBorder;
     public final boolean forceDisableWebviewGPU;
     
     public InternalSettings(Bundle settings) {
+    	if(settings == null){
+    		MyLog.w(TAG, "WARNING: settings is null!");
+    		disableUIContainerBorder = false;
+    		forceDisableWebviewGPU = false;
+    		return;
+    	}
+    	
         disableUIContainerBorder = settings.containsKey(DISABLE_UI_CONTAINER_BORDER)
                 ? settings.getBoolean(DISABLE_UI_CONTAINER_BORDER)
                 : false;
