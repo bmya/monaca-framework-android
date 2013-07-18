4b6091ef17cffe022c455437195bb739921d8803
Kruy Vanna <kruyvanna@gmail.com>
2013-07-18 16:05:27 +0900

    Android: add build and release scripts

---
+++ android/scripts/release.sh
@@ -0,0 +1,8 @@
+cd ..
+android update project -p MonacaUtils
+android update project -p cordova-android/framework
+android update project -p BarcodeScannerLibrary
+android update project -p MonacaFramework
+android update project -p MonacaSandbox
+cd MonacaSandbox
+ant clean release
