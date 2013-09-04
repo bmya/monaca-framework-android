8536911ed6733e111af71c48e7d5f0ca5df524cf
Kruy Vanna <kruyvanna@gmail.com>
2013-09-04 15:43:29 +0900

    add setup.sh for setting up android project using android command

---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+cd ..
+android update project -p MonacaUtils -t android-17
+android update project -p cordova-android/framework -t android-17
+android update project -p BarcodeScannerLibrary -t android-17
+android update project -p MonacaFramework -t android-17
+android update project -p MonacaFrameworkTest -t android-17
+android update project -p MonacaSandbox -t android-17
